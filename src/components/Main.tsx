9126962aafb3e91942612b121fcba2566ef0c21a
Caio Dias <caiocozendey@hotmail.com>
2023-06-29 14:54:20 -0300

    Terminando navbar, bug na coluna

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,20 +1,150 @@
-import FoodCards from "./FoodCards";
+import FoodCards from "./Card";
 
 export default function Principal() {
     return (
-        <div className="flex flex-col items-center">
-            <h1 className="text-4xl pt-8"> Petiscos </h1>
-            <div className="px-64 grid grid-cols-4 gap-4 pt-8">
-
-                <FoodCards />
-                <FoodCards />
-                <FoodCards />
-                <FoodCards />
-                <FoodCards />
-                <FoodCards />
-
+        <div className='items-center flex flex-col mx-16'>
+            <h1 className='text-4xl mt-4'> Components </h1>
+            <h2 className='text-2xl mt-4'> Cards </h2>
+            <div className="mt-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 lg:gap-12 md:gap-4 gap-8">
+                <FoodCards
+                    nome="Batata Frita"
+                    descricao="Batata frita!"
+                    preco="R$19,99"
+                    imagem="/batata-frita.jpg"
+                />
+                <FoodCards
+                    nome="Batata Frita"
+                    descricao="Batata frita!"
+                    preco="R$19,99"
+                    imagem="/batata-frita.jpg"
+                />
+                <FoodCards
+                    nome="Batata Frita"
+                    descricao="Batata frita!"
+                    preco="R$19,99"
+                    imagem="/batata-frita.jpg"
+                />
+                <FoodCards
+                    nome="Batata Frita"
+                    descricao="Batata frita!"
+                    preco="R$19,99"
+                    imagem="/batata-frita.jpg"
+                />
+                <FoodCards
+                    nome="Batata Frita"
+                    descricao="Batata frita!"
+                    preco="R$19,99"
+                    imagem="/batata-frita.jpg"
+                />
+                <FoodCards
+                    nome="Batata Frita"
+                    descricao="Batata frita!"
+                    preco="R$19,99"
+                    imagem="/batata-frita.jpg"
+                />
+                <FoodCards
+                    nome="Batata Frita"
+                    descricao="Batata frita!"
+                    preco="R$19,99"
+                    imagem="/batata-frita.jpg"
+                />
+                <FoodCards
+                    nome="Batata Frita"
+                    descricao="Batata frita!"
+                    preco="R$19,99"
+                    imagem="/batata-frita.jpg"
+                />
+                <FoodCards
+                    nome="Batata Frita"
+                    descricao="Batata frita!"
+                    preco="R$19,99"
+                    imagem="/batata-frita.jpg"
+                />
+                <FoodCards
+                    nome="Batata Frita"
+                    descricao="Batata frita!"
+                    preco="R$19,99"
+                    imagem="/batata-frita.jpg"
+                />
+                <FoodCards
+                    nome="Batata Frita"
+                    descricao="Batata frita!"
+                    preco="R$19,99"
+                    imagem="/batata-frita.jpg"
+                />
+                <FoodCards
+                    nome="Batata Frita"
+                    descricao="Batata frita!"
+                    preco="R$19,99"
+                    imagem="/batata-frita.jpg"
+                />
+                <FoodCards
+                    nome="Batata Frita"
+                    descricao="Batata frita!"
+                    preco="R$19,99"
+                    imagem="/batata-frita.jpg"
+                />
+                <FoodCards
+                    nome="Batata Frita"
+                    descricao="Batata frita!"
+                    preco="R$19,99"
+                    imagem="/batata-frita.jpg"
+                />
+                <FoodCards
+                    nome="Batata Frita"
+                    descricao="Batata frita!"
+                    preco="R$19,99"
+                    imagem="/batata-frita.jpg"
+                />
+                <FoodCards
+                    nome="Batata Frita"
+                    descricao="Batata frita!"
+                    preco="R$19,99"
+                    imagem="/batata-frita.jpg"
+                />
+                <FoodCards
+                    nome="Batata Frita"
+                    descricao="Batata frita!"
+                    preco="R$19,99"
+                    imagem="/batata-frita.jpg"
+                />
+                <FoodCards
+                    nome="Batata Frita"
+                    descricao="Batata frita!"
+                    preco="R$19,99"
+                    imagem="/batata-frita.jpg"
+                />
+                <FoodCards
+                    nome="Batata Frita"
+                    descricao="Batata frita!"
+                    preco="R$19,99"
+                    imagem="/batata-frita.jpg"
+                />
+                <FoodCards
+                    nome="Batata Frita"
+                    descricao="Batata frita!"
+                    preco="R$19,99"
+                    imagem="/batata-frita.jpg"
+                />
+                <FoodCards
+                    nome="Batata Frita"
+                    descricao="Batata frita!"
+                    preco="R$19,99"
+                    imagem="/batata-frita.jpg"
+                />
+                <FoodCards
+                    nome="Batata Frita"
+                    descricao="Batata frita!"
+                    preco="R$19,99"
+                    imagem="/batata-frita.jpg"
+                />
+                <FoodCards
+                    nome="Batata Frita"
+                    descricao="Batata frita!"
+                    preco="R$19,99"
+                    imagem="/batata-frita.jpg"
+                />
             </div>
-
         </div>
     )
 }
