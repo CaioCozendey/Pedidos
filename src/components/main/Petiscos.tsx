155deccff1687075bad6a988a3653c3fe2e6b5e5
Caio <caiocozendey@hotmail.com>
2023-07-09 22:42:36 -0300

    corrigindo erro navbar

--- src/components/main/Petiscos.tsx
+++ src/components/main/Petiscos.tsx
@@ -3,7 +3,7 @@ import Card from '../cards/Index'
 export default function Petiscos() {
     return (
         <div>
-            <h1 className='text-white text-5xl mt-6 mb-6 font-semibold text-center' id='pizza'> Petisco </h1>
+            <h1 className='text-white text-5xl mt-6 mb-6 font-semibold text-center' id='petisco'> Petisco </h1>
             <div className="mt-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 lg:gap-12 md:gap-4 gap-8">
                 <Card
                     nome={'Carne Filetada'}
